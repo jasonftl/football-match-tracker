b3654f5e94576a036b76f74f7ae135f156c59e48
Jason Gordon <jason@gigglebyte.net>
2025-10-05 22:05:38 +0100

    Add new files and changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MatchTracker from './components/MatchTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <MatchTracker />
+    </div>
+  );
+}
+
+export default App;
